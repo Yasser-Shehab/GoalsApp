78be1f044579fe0856f34151844c9ef5f7505f09
AquaApple <yassershehab9@gmail.com>
2022-04-01 13:25:39 +0200

    Initial Crud Commit for Goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,17 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { getGoals, postGoals, updateGoals, deleteGoals } = require("../controllers/goalController");
 
-router.get("/goals", (req, res) => {
-  res.status(200).send("Get Request is Successful");
-});
-router.post("/goals", (req, res) => {
-  res.status(200).send("Set Goal");
-});
-router.put("/:id", (req, res) => {
-  res.status(200).send(`Get Goal ${req.params.id}`);
-});
-router.delete("/:id", (req, res) => {
-  res.status(200).send(`Delete Goal ${req.params.id}`);
-});
+router.route("/").get(getGoals).post(postGoals);
+router.route("/:id").put(updateGoals).delete(deleteGoals);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
-const port = process.env.PORT;
+const colos = require("colors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 const goalRouter = require("./routes/goalRoutes");
 
-app.use("/api", goalRouter);
+connectDB();
 
-app.listen(port, () => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", goalRouter);
+app.use(errorHandler);
+
+app.listen(port, async () => {
   console.log(`Listing On ${port}`);
 });
