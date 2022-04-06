452659cd2e47d313970de81c74cd68fdd747b3c0
AquaApple <yassershehab9@gmail.com>
2022-04-06 14:44:32 +0200

    Added production build

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 const app = express();
-const colos = require("colors");
+const colors = require("colors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 const connectDB = require("./config/db");
 const goalRouter = require("./routes/goalRoutes");
 const userRouter = require("./routes/userRoutes");
+
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please switch to production"));
+}
 
 connectDB();
 
