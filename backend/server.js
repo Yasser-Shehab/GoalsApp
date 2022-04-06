ed5af3bf5d3ecdaed0485c13af527c63d106e941
AquaApple <yassershehab9@gmail.com>
2022-04-06 15:52:13 +0200

    Moved serving the frontend to below the routes

--- backend/server.js
+++ backend/server.js
@@ -10,17 +10,6 @@ const connectDB = require("./config/db");
 const goalRouter = require("./routes/goalRoutes");
 const userRouter = require("./routes/userRoutes");
 
-//Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please switch to production"));
-}
-
 connectDB();
 
 app.use(cors());
@@ -29,6 +18,18 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRouter);
 app.use("/api/users", userRouter);
+
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../", "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 app.use(errorHandler);
 
 app.listen(port, async () => {
